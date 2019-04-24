84804148f40042017dfc45c55c205a5953512f09
Luis Puhl <luispuhl@gmail.com>
2019-04-24 00:25:17 -0300

    adding clustering algorithm

--- minas.py
+++ minas.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn as sk
+from sklearn.cluster import KMeans
 
 class Example:
   label = None
@@ -41,20 +46,20 @@ class Minas(Model):
     self.model = model
   
   """
-  Require:
-    k: number of micro-clusters,
-    alg: clustering algorithm,
-    S: Training Set
-  
-  Model ← ∅
-  for all (class Ci in S) do
-    ModelTmp ← Clustering(SClass=Ci, k, alg)
-    for all (micro-cluster micro in ModelTmp) do
-      micro.label ← Ci ;
+    Require:
+      k: number of micro-clusters,
+      alg: clustering algorithm,
+      S: Training Set
+    
+    Model ← ∅
+    for all (class Ci in S) do
+      ModelTmp ← Clustering(SClass=Ci, k, alg)
+      for all (micro-cluster micro in ModelTmp) do
+        micro.label ← Ci ;
+      end for
+      Model ← Model ∪ ModelTmp;
     end for
-    Model ← Model ∪ ModelTmp;
-  end for
-  return Model
+    return Model
   """
   def offline(self, training_set=[]):
     assert len(training_set) > 0
@@ -68,89 +73,115 @@ class Minas(Model):
         clusters = self.clustering(current_examples)
         for cluster in clusters:
           cluster.label = current_label
-        self.clusters.extend(cluster)
+        self.clusters.extend(clusters)
         current_examples = []
         current_label = example.label
       current_examples.append(example)
     return self
   
   """
-  Require:
-    Model: decision model from initial training phase,
-    DS: data stream,
-    T: threshold,
-    NumExamples: minimal number of examples to execute a ND procedure,
-    windowsize: size of a data window,
-    alg: clustering algorithm
-  
-  ShortMem ← ∅
-  SleepMem ← ∅
-  
-  for all (example ex in DS) do
-    (Dist, micro) ← closer-micro(ex,Model)
-    if (Dist ≤ radius(micro) then
-      ex.class ← micro.label
-      update-micro(micro,ex)
-    else
-      ex.class ← unknown
-      ShortMem ← ShortMem ∪ ex
-      if (|ShortMem| ≥ NumExamples) then
-        Model ← novelty-detection(Model, ShortMem, SleepMem, T, alg)
+    Require:
+      Model: decision model from initial training phase,
+      DS: data stream,
+      T: threshold,
+      NumExamples: minimal number of examples to execute a ND procedure,
+      windowsize: size of a data window,
+      alg: clustering algorithm
+    
+    ShortMem ← ∅
+    SleepMem ← ∅
+    
+    for all (example ex in DS) do
+      (Dist, micro) ← closer-micro(ex,Model)
+      if (Dist ≤ radius(micro) then
+        ex.class ← micro.label
+        update-micro(micro,ex)
+      else
+        ex.class ← unknown
+        ShortMem ← ShortMem ∪ ex
+        if (|ShortMem| ≥ NumExamples) then
+          Model ← novelty-detection(Model, ShortMem, SleepMem, T, alg)
+        end if
       end if
-    end if
-    CurrentTime ← ex.time
-    if (CurrentTime mod windowSize == 0) then
-      Model ← move-sleepMem(Model, SleepMem, CurrentTime, windowSize)
-      ShortMem ← remove-oldExamples(ShortMem, windowsize)
-    end if
-  end for
+      CurrentTime ← ex.time
+      if (CurrentTime mod windowSize == 0) then
+        Model ← move-sleepMem(Model, SleepMem, CurrentTime, windowSize)
+        ShortMem ← remove-oldExamples(ShortMem, windowsize)
+      end if
+    end for
   """
   def online(self, k, training_set):
     pass
 
   """
-  Require:
-    Model: current decision model,
-    ShortMem: short-term memory,
-    SleepMem: sleep memory,
-    T: threshold,
-    alg: clustering algorithm
-  
-  ModelTmp ← Clustering(ShortMem, k, alg)
-  for all (micro-grupo micro in ModelTemp) do
-    if ValidationCriterion(micro) then
-      (Dist, microM) ← closest-micro(micro,Model)
-      if Dist ≤ T then
-        micro.label ← microM.label
-      else
-        (Dist, microS) ← closest-micro(micro,SleepMem)
+    Require:
+      Model: current decision model,
+      ShortMem: short-term memory,
+      SleepMem: sleep memory,
+      T: threshold,
+      alg: clustering algorithm
+    
+    ModelTmp ← Clustering(ShortMem, k, alg)
+    for all (micro-grupo micro in ModelTemp) do
+      if ValidationCriterion(micro) then
+        (Dist, microM) ← closest-micro(micro,Model)
         if Dist ≤ T then
-          micro.label ← microS.label
+          micro.label ← microM.label
         else
-          micro.label ← new label
+          (Dist, microS) ← closest-micro(micro,SleepMem)
+          if Dist ≤ T then
+            micro.label ← microS.label
+          else
+            micro.label ← new label
+          end if
         end if
+        Model ← Model ∪ micro
       end if
-      Model ← Model ∪ micro
-    end if
-  end for
-  return Model
+    end for
+    return Model
   """
   def noveltyDetection(self, parameter_list):
     pass
 
-  def clustering(self, parameter_list):
-    raise NotImplementedError
+  def clustering(self, examples):
+    assert len(examples) > 0
+    #
+    # Data = {
+    #   'x': [25,34,22,27,33,33,31,22,35,34,67,54,57,43,50,57,59,52,65,47,49,48,35,33,44,45,38,43,51,46],
+    #   'y': [79,51,53,78,59,74,73,57,69,75,51,32,40,47,53,36,35,58,59,50,25,20,14,12,20,5,29,27,8,7]
+    # }
+    df = pd.DataFrame(data=[ex.item for ex in examples])
+    
+    n_samples = len(examples)
+    n_clusters = self.k
+    if n_samples < n_clusters:
+      n_clusters = int(n_samples / 10)
+    print(n_samples, n_clusters)
+    assert n_samples >= n_clusters
+    kmeans = KMeans(n_clusters=n_clusters).fit(df)
+    centroids = kmeans.cluster_centers_
+    print(centroids, kmeans.inertia_, kmeans.labels_)
 
-if __name__ == "__main__":
+    plt.scatter(df[0], df[1], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
+    plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
+    plt.show()
+
+    clusters = []
+    for centroid in kmeans.cluster_centers_:
+      c = Cluster()
+      c.center = centroid
+      clusters.append(c)
+
+    return clusters
+
+def selfTest():
   print('Running self tests')
   # setup fake examples
-  import random as random
-  import numpy as np
   attributes = np.random.randint(2, 40)
   examples = []
   for labelIndex in range(np.random.randint(2, 5)):
-      mu = random.random() * 10
-      sigma = random.random() * 5
+      mu = np.random.random() * 10
+      sigma = np.random.random() * 5
       for exampleIndex in range(np.random.randint(200, 1000)):
           example = Example()
           example.index = labelIndex
@@ -158,23 +189,25 @@ if __name__ == "__main__":
           example.item = [np.random.normal(loc=mu, scale=sigma) for i in range(attributes)]
           examples.append(example)
   np.random.shuffle(examples)
+  plotExamples2D(examples)
   #
   minas = Minas()
   minas.offline(examples[:int(len(examples) * .1)])
 
 def plotExamples2D(examples):
-  import matplotlib.pyplot as plt
-
   fig, ax = plt.subplots()
   for i in set([ex.index for ex in examples]):
       exs = [ex for ex in examples if ex.index == i]
       x=np.array([ex.item[0] for ex in exs])
       y=np.array([ex.item[1] for ex in exs])
       label=[ex.label for ex in examples if ex.index == i][0]
-      print(i, len(exs), len(x), len(y), c, label)
+      print(i, len(exs), len(x), len(y), label)
       ax.scatter(x=x, y=y, label=label)
 
   ax.legend()
   ax.grid(True)
 
   plt.show()
+
+if __name__ == "__main__":
+  selfTest()
