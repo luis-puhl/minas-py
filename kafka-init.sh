42d87d990331110c667556668f4426ab56fafe3e
Luis Puhl <luispuhl@gmail.com>
2019-06-14 19:26:43 -0300

    Testing a wait for the shell kafka-init file

--- kafka-init.sh
+++ kafka-init.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 ./kafka/bin/zookeeper-server-start.sh kafka.config/zookeeper.properties &
+pids[${i}]=$!
 sleep 10
 
 ./kafka/bin/kafka-server-start.sh kafka.config/servers/server.0.properties &
+pids[${i}]=$!
 ./kafka/bin/kafka-server-start.sh kafka.config/servers/server.1.properties &
+pids[${i}]=$!
 ./kafka/bin/kafka-server-start.sh kafka.config/servers/server.2.properties &
+pids[${i}]=$!
 sleep 10
 
-./kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 16 --topic minas-topic
+./kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --topic items --create --replication-factor 1 --partitions 16 &
+pids[${i}]=$!
+./kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --topic clusters --create --replication-factor 1 --partitions 16 &
+pids[${i}]=$!
+
+# wait for all pids
+for pid in ${pids[*]}; do
+    sleep 10
+    wait $pid
+done
+
+for job in `jobs -p`
+do
+    echo $job
+    wait $job || echo "FAIL $job"
+done
+
 # ./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic minas-topic --from-beginning
-export ZK_HOSTS="localhost:2181"
-./kafka-manager/bin/kafka-manager
+# export ZK_HOSTS="localhost:2181"
+# ./kafka-manager/bin/kafka-manager
