af539dafb8bb86a8c5b1d33bcc637dddcbc9e048
Luis Puhl <luispuhl@gmail.com>
2019-08-15 17:31:48 -0300

    Streams example working, had to version bump javac and tycho JDTc.

--- kafka-streams/streams.examples/src/main/java/myapps/Pipe.java
+++ kafka-streams/streams.examples/src/main/java/myapps/Pipe.java
@@ -8,14 +8,23 @@ import org.apache.kafka.streams.Topology;
 
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
+import org.apache.kafka.streams.kstream.KStream;
 
 public class Pipe {
     public static void main(String[] args) throws Exception {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CONFIG, Serdes,String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CONFIG, Serdes,String().getClass());
-        // StreamsConfig
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        //
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+        source.to("streams-pipe-output");
+        //
+        final Topology topology = builder.build();
+        System.out.println(topology.describe());
+        System.out.println("Hello");
+        
     }
 }
