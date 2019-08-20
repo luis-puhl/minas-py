0800e5258a921bd9419c55432dc18fb1704b2715
Luis Puhl <luispuhl@gmail.com>
2019-08-20 16:22:40 -0300

    Streams tutorial, wordcount final.

--- kafka-streams/streams.examples/src/main/java/myapps/WordCount.java
+++ kafka-streams/streams.examples/src/main/java/myapps/WordCount.java
@@ -32,45 +32,19 @@ public class WordCount {
         props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, cores);
         //
         final StreamsBuilder builder = new StreamsBuilder();
+ 
         KStream<String, String> source = builder.stream("streams-plaintext-input");
-        Locale locale = Locale.getDefault();
-        KStream<String, String> words = source.flatMapValues(
-                value -> Arrays.asList(value.toLowerCase(locale).split("\\W+"))
-        );
-        KGroupedStream<String, String> wordsGrouped = words.groupBy(
-                (key, value) -> value
-        );
-        // Materialize the result into a KeyValueStore named "counts-store".
-        // The Materialized store is always of type <Bytes, byte[]> as this is the format of the inner most store.
-        Materialized<String, Long, KeyValueStore<Bytes, byte[]>> materialized;
-        materialized = Materialized.<String, Long, KeyValueStore<Bytes, byte[]>> as("counts-store");
-        KTable<String, Long> count = wordsGrouped.count(materialized);
-        count.toStream().to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
-        /*
-        KStream<String, String> words = source.flatMapValues(new ValueMapper<String, Iterable<String>>() {
-            @Override
-            public Iterable<String> apply(String value) {
-                value = value.toLowerCase(locale);
-                return Arrays.asList(value.split("\\W+"));
-            }
-        });
-        */
-        /*
-        words.groupBy(new KeyValueMapper<String, String, String>() {
-        @Override
-        public Object apply(String key, String value) {
-        return value;
-        }
-        });
-         */
-        //
+        source.flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
+              .groupBy((key, value) -> value)
+              .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"))
+              .toStream()
+              .to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
+ 
         final Topology topology = builder.build();
         System.out.println(topology.describe());
-        // 
         final KafkaStreams streams = new KafkaStreams(topology, props);
-        //
-        final CountDownLatch latch = new CountDownLatch(1);
         // attach shutdown handler to catch control-c
+        final CountDownLatch latch = new CountDownLatch(1);
         Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
             @Override
             public void run() {
